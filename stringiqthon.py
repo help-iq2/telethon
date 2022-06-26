90dd64f26f2622378202fb0c9e4ae8bb8447609a
help-iq2 <102095819+help-iq2@users.noreply.github.com>
2022-06-26 18:31:49 +0300

    Update and rename stringsetup.py to stringiqthon.py

--- stringiqthon.py
+++ stringiqthon.py
@@ -7,14 +7,9 @@
 from telethon.sessions import StringSession
 from telethon.sync import TelegramClient
 
-print(
-    """Please go-to my.telegram.org
-Login using your Telegram account
-Click on API Development Tools
-Create a new application, by entering the required details"""
-)
-APP_ID = int(input("Enter APP ID here: "))
-API_HASH = input("Enter API HASH here: ")
+print("- Code telethon For ch : @iqthon")
+APP_ID = int(input("APP ID here: "))
+API_HASH = input("API HASH here: ")
 
 with TelegramClient(StringSession(), APP_ID, API_HASH) as client:
     print(client.session.save())
